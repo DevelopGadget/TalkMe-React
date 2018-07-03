9dc4d51cd56518a274798fae232d6616edb9d594
FernanPro18 <fernandoaraujo081@gmail.com>
2018-07-02 22:25:54 -0500

    Probando

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { Button, Icon, Row, Input } from 'react-materialize'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Row>
+        <Input placeholder="Placeholder" s={6} label="First Name" />
+        <Input s={6} label="Last Name" />
+        <Input s={12} label="disabled" defaultValue="I am not editable" disabled />
+        <Input type="password" label="password" s={12} />
+        <Input type="email" label="Email" s={12} />
+      </Row>
     );
   }
 }
