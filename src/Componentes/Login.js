5f065c860be2f3ec83f1d0548a7e1700fdac774e
FernanPro18 <fernandoaraujo081@gmail.com>
2018-07-05 22:37:46 -0500

    Botones

--- src/Componentes/Login.js
+++ src/Componentes/Login.js
@@ -4,13 +4,18 @@ import '../Css/TextInput.css';
 import { Button, Icon, Row, Input, Container, Col } from 'react-materialize';
 
 class Login extends Component {
+
   constructor(props) {
     super(props);
     this.state = { Email: '', Password: '' }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     document.getElementById('Input').focus();
+  }
+
+  Login = async () => {
+
   }
 
   render() {
@@ -19,18 +24,26 @@ class Login extends Component {
         <Col className='center-align'>
           <Row className='center-align' >
             <Col className='left-align' s={12} m={12} l={8} offset={'l2'}>
-              <Input s={12}  label='Email' type='email' validate={true} onChange={(Email) => this.setState({ Email: Email })} error={'Email invalido'} id={'Input'} success={'Email Valido'}>
+              <Input s={12} label='Email' type='email' validate={true} onChange={(Email) => this.setState({ Email: Email })} error={'Email invalido'} id={'Input'} success={'Email Valido'}>
                 <Icon large>alternate_email</Icon>
               </Input>
             </Col>
           </Row>
           <Row className='center-align'>
             <Col className='left-align' s={12} m={12} l={8} offset={'l2'}>
-              <Input s={12}  label='Contraseña' type='password' validate={true} onChange={(Password) => this.setState({ Password: Password })}><Icon large>vpn_key</Icon></Input>
+              <Input s={12} label='Contraseña' type='password' validate={true} onChange={(Password) => this.setState({ Password: Password })}>
+                <Icon large>vpn_key</Icon>
+              </Input>
+              <Button waves='purple' flat={true} style={{ backgroundColor: 'transparent' }}>Cambiar Contraseña</Button>
             </Col>
           </Row>
           <Row>
-            <Button waves='red' type='submit' large id={'boton'}>Login<Icon right>send</Icon></Button>
+            <Row>
+              <Button waves='red' type='submit' large>Login<Icon right>send</Icon></Button>
+            </Row>
+            <Row>
+              <Button waves='purple' flat={true} style={{ backgroundColor: 'transparent' }}>¿NO TIENES CUENTA?</Button>
+            </Row>
           </Row>
         </Col>
       </Container >
